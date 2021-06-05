0723e4dfe3c27a7e18d9d2c52393bbfbcdbf06f8
Surbhi_Official <79106894+SurbhiOfficial@users.noreply.github.com>
2021-06-05 13:26:21 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  let n = new Date();
+  let v = n.getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {v}</p>
+    </footer>
+  );
+}
+
+export default Footer;
